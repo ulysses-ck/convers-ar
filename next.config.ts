f5403eeae6405d67493cd4a5cac0c64cce7f6349
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-09 18:21:34 -0300

    chore: remove cf pages integration

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-(async () => {
-  if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
-})();
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
