f0912626b606623d79c20611fa82267328772006
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-09 19:19:17 -0300

    feat: add footer component with educational purpose notice

---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="absolute bottom-0 w-full text-center">
+			<p className="text-[10px]">2025 ulysses-ck - This project is for educational purposes only and is not intended for commercial use.</p>
+		</footer>
+	);
+}
